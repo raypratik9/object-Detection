952f7afb92b99272a60f12929a74e1e60bff38ee
pratik ray <50998329+raypratik9@users.noreply.github.com>
2020-04-19 17:59:34 +0530

    Update yolo_object_detection.py

--- object detection with Yolo_Opencv/yolo_object_detection.py
+++ object detection with Yolo_Opencv/yolo_object_detection.py
@@ -4,7 +4,7 @@ import numpy as np
 # Load Yolo
 net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
 classes = []
-with open("coco.names", "r") as f:
+with open("label.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
